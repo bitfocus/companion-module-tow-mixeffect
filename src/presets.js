1edf021f8d8c344ec3976378d293592b7ebabcdf
Johnny Estilles <johnny@estilles.com>
2021-09-05 02:27:47 +0800

    refactoring: preset generation
    Signed-off-by: Johnny Estilles <johnny@estilles.com>

--- src/presets.js
+++ src/presets.js
@@ -4,355 +4,210 @@ module.exports = {
 	initPresets() {
 		const presets = []
 
-		const addPresets = (category) => category.list.forEach(item => presets.push(category.generate(item)))
-
-		const generateWithNoOptions = ({ category, label, size, action }) => ({
-			category: category,
-			label: label,
+		const generatePreset = ({ category, label, size = 14, action, bank = {}, options = {} }) => ({
+			category,
+			label,
 			bank: {
 				style: 'text',
 				text: label,
-				size: size,
+				size,
 				color: this.rgb(255, 255, 255),
 				bgcolor: this.rgb(0, 0, 0),
+				...bank,
 			},
 			actions: [
 				{
-					action: action,
+					action,
+					options,
 				},
 			],
 		})
 
-		const switcher = {
+		const addPresetList = ({ category, size = 14, action, bank = {}, list = [] }) => list.forEach(item => presets.push(generatePreset({
+			category, 
+			size,
+			action,
+			...item,
+			bank: {
+				...bank,
+				...item.bank,
+			},
+		})))
+
+		// Switcher
+		const switcherSectionList = [
+			{ label: 'Audio', options: { section: 'audio' } },
+			{ label: 'AUX', options: { section: 'auxiliary' } },
+			{ label: 'Camera Control', options: { section: 'camera-control' } },
+			{ label: 'Color Gen', options: { section: 'color-generators' } },
+			{ label: 'DSK', options: { section: 'downstream-keyers' } },
+			{ label: 'Hyperdeck', options: { section: 'hyperdecks' } },
+			{ label: 'Macros', options: { section: 'macros' } },
+			{ label: 'Media', options: { section: 'media' } },
+			{ label: 'Output', options: { section: 'output' } },
+			{ label: 'Settings', options: { section: 'settings' } },
+			{ label: 'Super Source', options: { section: 'supersource' } },
+			{ label: 'Switcher', options: { section: 'switcher' } },
+			{ label: 'Transition', options: { section: 'transitions' } },
+			{ label: 'USK', options: { section: 'upstream-keyers' } },
+			{ label: 'View All Presets', options: { section: 'view-all' } },
+		]
+		addPresetList({ 
 			category: 'Switcher',
-			list: [
-				{ id: 'audio', label: 'Audio', size: 14 },
-				{ id: 'auxiliary', label: 'AUX', size: 14 },
-				{ id: 'camera-control', label: 'Camera Control', size: 14 },
-				{ id: 'color-generators', label: 'Color Gen', size: 14 },
-				{ id: 'downstream-keyers', label: 'DSK', size: 14 },
-				{ id: 'hyperdecks', label: 'Hyperdeck', size: 14 },
-				{ id: 'macros', label: 'Macros', size: 14 },
-				{ id: 'media', label: 'Media', size: 14 },
-				{ id: 'output', label: 'Output', size: 14 },
-				{ id: 'settings', label: 'Settings', size: 14 },
-				{ id: 'supersource', label: 'Super Source', size: 14 },
-				{ id: 'switcher', label: 'Switcher', size: 14 },
-				{ id: 'transitions', label: 'Transition', size: 14 },
-				{ id: 'upstream-keyers', label: 'USK', size: 14 },
-				{ id: 'view-all', label: 'View All Presets', size: 14 },
-			],
-			generate: (item) => ({
-				category: switcher.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: item.size,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: 'switcherSection',
-						options: {
-							section: item.id,
-						},
-					},
-				],
-			}),
-		}
-
-		const transitions = {
-			category: 'Transitions',
-			list: [
-				{ action: 'auto', label: 'Auto' },
-				{ action: 'cut', label: 'Cut' },
-			],
-			generate: (item) => ({
-				category: transitions.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: 18,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: item.action,
-						options: {
-							meID: 1,
-						},
-					},
-				],
-			}),
-		}
-
-
-		const superSourcePreset = {
-			category: 'SuperSource Preset',
-			list: [
-				{ label: 'Four Grid' },
-				{ label: 'Four Grid Alternate' },
-				{ label: 'Four Grid Full' },
-				{ label: 'Four Horizontal Crop' },
-				{ label: 'Four Horizontal Full' },
-				{ label: 'Four Left' },
-				{ label: 'Four Left Full' },
-				{ label: 'iPad Mini Left' },
-				{ label: 'iPad Mini PIP' },
-				{ label: 'iPad Mini Right' },
-				{ label: 'iPad Pro 12.9 Left' },
-				{ label: 'iPad Pro 12.9 PIP' },
-				{ label: 'iPad Pro 12.9 Right' },
-				{ label: 'iPhone Pro Max PIP' },
-				{ label: 'PIP Under' },
-				{ label: 'Standard - 1' },
-				{ label: 'Standard - 2' },
-				{ label: 'Standard - 3' },
-				{ label: 'Standard - 4' },
-				{ label: 'Three Horizontal Crop' },
-				{ label: 'Three Horizontal Fill' },
-				{ label: 'Three Left' },
-				{ label: 'Three Left Crop' },
-				{ label: 'Three Top' },
-				{ label: 'Two Bottom' },
-				{ label: 'Two Diagonal' },
-				{ label: 'Two Horizontal' },
-				{ label: 'Two Horizontal Crop' },
-				{ label: 'Two Horizontal Fill' },
-				{ label: 'Two Left' },
-				{ label: 'Two Right' },
-				{ label: 'Two Top' },
-				{ label: 'Two Vertical' },
-				{ label: 'Two Vertical Fill' },
-				{ label: 'USK - Five Grid Corp' },
-				{ label: 'USK - Five Left' },
-				{ label: 'USK - PIP Over' },
-				{ label: 'USK - PIP Over Two' },
-				{ label: 'USK - Six Grid' },
-				{ label: 'USK - Six Grid Crop' },
-				{ label: 'USK - Six Left' },
-			],
-			generate: (item) => ({
-				category: superSourcePreset.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					alignment: 'center:bottom',
-					size: 7,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-					png64: thumbnails[item.label],
-					pngalignment: 'center:bottom',
-				},
-				actions: [
-					{
-						action: 'superSourcePreset',
-						options: {
-							presetName: item.label,
-							superSourceId: 1,
-						},
-					},
-				],
-			}),
-		}
-
-		const superSourcePresetAdditional = {
-			category: 'SuperSource Preset',
-			list: [
-				{ action: 'superSourcePresetPrevious', label: 'Previous Preset' },
-				{ action: 'superSourcePresetNext', label: 'Next  Preset' },
-			],
-			generate: (item) => ({
-				category: superSourcePresetAdditional.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: 14,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: item.action,
-						options: {
-							superSourceId: 1,
-						},
-					},
-				],
-			}),
-		}
-
-		const superSourceAnimationSpeed = {
-			category: 'SuperSource Animation Speed',
-			list: [
-				{ id: 0, label: 'Instant' },
-				{ id: 1, label: 'Extra Fast' },
-				{ id: 2, label: 'Fast' },
-				{ id: 3, label: 'Normal' },
-				{ id: 4, label: 'Slow' },
-				{ id: 5, label: 'Extra Slow' },
-			],
-			generate: (item) => ({
-				category: superSourceAnimationSpeed.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: '18',
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: 'superSourceAnimationSpeed',
-						options: {
-							speed: item.id,
-						},
-					},
-				],
-			}),
-		}
-
-		const superSourceAnimationStyle = {
-			category: 'SuperSource Animation Style',
-			list: [
-				{ id: 0, label: 'Cosine' },
-				{ id: 1, label: 'Cubed' },
-				{ id: 2, label: 'Inverse Cubed' },
-				{ id: 3, label: 'Inverse Squared' },
-				{ id: 4, label: 'Linear' },
-				{ id: 5, label: 'Sine' },
-				{ id: 6, label: 'Smooth Step' },
-				{ id: 7, label: 'Smoother Step' },
-				{ id: 8, label: 'Squared' },
-			],
-			generate: (item) => ({
-				category: superSourceAnimationStyle.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: 14,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: 'superSourceAnimationStyle',
-						options: {
-							style: item.id,
-						},
-					},
-				],
-			}),
-		}
-
-		const superSourceHighlight = {
-			category: 'SuperSource Highlight',
-			list: [
-				{ id: 1, label: 'Highlight Box 1' },
-				{ id: 2, label: 'Highlight Box 2' },
-				{ id: 3, label: 'Highlight Box 3' },
-				{ id: 4, label: 'Highlight Box 4' },
-			],
-			generate: (item) => ({
-				category: superSourceHighlight.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: 14,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: 'superSourceHighlight',
-						options: {
-							boxId: item.id,
-							superSourceId: 1,
-						},
-					},
-				],
-			}),
-		}
-
-		const videoFollowAudio = {
-			category: 'Video Follows Audio',
-			list: [
-				{ id: 'on', label: 'VFA\\nOn' },
-				{ id: 'off', label: 'VFA\\nOff' },
-				{ id: 'toggle', label: 'VFA\\nToggle' },
-			],
-			generate: (item) => ({
-				category: videoFollowAudio.category,
-				label: item.label,
-				bank: {
-					style: 'text',
-					text: item.label,
-					size: 18,
-					color: this.rgb(255, 255, 255),
-					bgcolor: this.rgb(0, 0, 0),
-				},
-				actions: [
-					{
-						action: 'videoFollowAudio',
-						options: {
-							mode: item.id,
-						},
-					},
-				],
-			}),
-		}
-
-		// Switcher 
-		addPresets(switcher)
+			action: 'switcherSection',
+			list: switcherSectionList ,
+		})
 
 		// Transitions
-		addPresets(transitions)
-
-		// SuperSource Presets
-		addPresets(superSourcePreset)
-		addPresets(superSourcePresetAdditional)
-		
-		// SuperSource Animation Speed
-		addPresets(superSourceAnimationSpeed)
-		presets.push(generateWithNoOptions({
-			category: superSourceAnimationSpeed.category,
-			label: 'Cycle Speed',
+		const transitionList = [
+			{ label: 'Auto', action: 'auto' },
+			{ label: 'Cut', action: 'cut' },
+		]
+		addPresetList({
+			category: 'Transitions',
 			size: 18,
-			action: 'superSourceAnimationSpeedCycle',
-		}))
-		
-		// SuperSource Animation Style
-		addPresets(superSourceAnimationStyle)
-		presets.push(generateWithNoOptions({
-			category: superSourceAnimationStyle.category,
-			label: 'Cycle Style',
+			list: transitionList,
+		})
+
+		// SuperSource
+		const superSourceList1 = [
+			{ label: 'Four Grid' },
+			{ label: 'Four Grid Alternate' },
+			{ label: 'Four Grid Full' },
+			{ label: 'Four Horizontal Crop' },
+			{ label: 'Four Horizontal Full' },
+			{ label: 'Four Left' },
+			{ label: 'Four Left Full' },
+			{ label: 'iPad Mini Left' },
+			{ label: 'iPad Mini PIP' },
+			{ label: 'iPad Mini Right' },
+			{ label: 'iPad Pro 12.9 Left' },
+			{ label: 'iPad Pro 12.9 PIP' },
+			{ label: 'iPad Pro 12.9 Right' },
+			{ label: 'iPhone Pro Max PIP' },
+			{ label: 'PIP Under' },
+			{ label: 'Standard - 1' },
+			{ label: 'Standard - 2' },
+			{ label: 'Standard - 3' },
+			{ label: 'Standard - 4' },
+			{ label: 'Three Horizontal Crop' },
+			{ label: 'Three Horizontal Fill' },
+			{ label: 'Three Left' },
+			{ label: 'Three Left Crop' },
+			{ label: 'Three Top' },
+			{ label: 'Two Bottom' },
+			{ label: 'Two Diagonal' },
+			{ label: 'Two Horizontal' },
+			{ label: 'Two Horizontal Crop' },
+			{ label: 'Two Horizontal Fill' },
+			{ label: 'Two Left' },
+			{ label: 'Two Right' },
+			{ label: 'Two Top' },
+			{ label: 'Two Vertical' },
+			{ label: 'Two Vertical Fill' },
+			{ label: 'USK - Five Grid Corp' },
+			{ label: 'USK - Five Left' },
+			{ label: 'USK - PIP Over' },
+			{ label: 'USK - PIP Over Two' },
+			{ label: 'USK - Six Grid' },
+			{ label: 'USK - Six Grid Crop' },
+			{ label: 'USK - Six Left' },
+		].map(({ label }) => (
+			{ label, bank: { png64: thumbnails[label] }, options: { presetName: label, superSourceId: 1 } }
+		))
+		addPresetList({
+			category: 'SuperSource Preset',
+			size: 7,
+			bank: {
+				alignment: 'center:bottom',
+				pngalignment: 'center:bottom',
+			},
+			action: 'superSourcePreset',
+			list: superSourceList1,
+		})
+
+		const superSourceList2 = [
+			{ label: 'Previous Preset', action: 'superSourcePresetPrevious' },
+			{ label: 'Next  Preset', action: 'superSourcePresetNext',  },
+		]
+		addPresetList({
+			category: 'SuperSource Preset',
 			size: 14,
-			action: 'superSourceAnimationStyleCycle',
-		}))
-		
+			list: superSourceList2,
+		})
+
+		// SuperSource Animation Speed
+		const superSourceAnimationSpeedList = [
+			{ label: 'Instant', action: 'superSourceAnimationSpeed', options: { speed: 0 } },
+			{ label: 'Extra Fast', action: 'superSourceAnimationSpeed', options: { speed: 1 }  },
+			{ label: 'Fast', action: 'superSourceAnimationSpeed', options: { speed: 2 }  },
+			{ label: 'Normal', action: 'superSourceAnimationSpeed', options: { speed: 3 }  },
+			{ label: 'Slow', action: 'superSourceAnimationSpeed', options: { speed: 4 }  },
+			{ label: 'Extra Slow', action: 'superSourceAnimationSpeed', options: { speed: 5 }  },
+			{ label: 'Cycle Speed', action: 'superSourceAnimationSpeedCycle' }
+		]
+		addPresetList({
+			category: 'SuperSource Animation Speed',
+			size: 18,
+			list: superSourceAnimationSpeedList,
+		})
+
+		// SuperSource Animation Style
+		const superSourceAnimationStyleList = [
+			{ label: 'Cosine', action: 'superSourceAnimationStyle', options: { style: 0 } },
+			{ label: 'Cubed', action: 'superSourceAnimationStyle', options: { style: 1 } },
+			{ label: 'Inverse Cubed', action: 'superSourceAnimationStyle', options: { style: 2 } },
+			{ label: 'Inverse Squared', action: 'superSourceAnimationStyle', options: { style: 3 } },
+			{ label: 'Linear', action: 'superSourceAnimationStyle', options: { style: 4 } },
+			{ label: 'Sine', action: 'superSourceAnimationStyle', options: { style: 5 } },
+			{ label: 'Smooth Step', action: 'superSourceAnimationStyle', options: { style: 6 } },
+			{ label: 'Smoother Step', action: 'superSourceAnimationStyle', options: { style: 7 } },
+			{ label: 'Squared', action: 'superSourceAnimationStyle', options: { style: 8 } },
+			{ label: 'Cycle Style', action: 'superSourceAnimationStyleCycle' }
+		]
+		addPresetList({
+			category: 'SuperSource Animation Style',
+			size: 18,
+			list: superSourceAnimationStyleList,
+		})
+
 		// SuperSource Highlight
-		addPresets(superSourceHighlight)
-		superSourceHighlight.list.forEach(item => presets.push(superSourceHighlight.generate(item)))
-		
+		superSourceHighlightList = [
+			{ label: 'Highlight Box 1', options: { boxId: 1 } },
+			{ label: 'Highlight Box 2', options: { boxId: 2 } },
+			{ label: 'Highlight Box 3', options: { boxId: 3 } },
+			{ label: 'Highlight Box 4', options: { boxId: 4 } },
+			{ label: 'Highlight Reset', options: { boxId: 0 } },
+		]
+		addPresetList({
+			category: 'SuperSource Highlight',
+			action: 'superSourceHighlight',
+			options: { superSourceId: 1 },
+			list: superSourceHighlightList,
+		})
+
 		// Video Follows Audio
-		addPresets(videoFollowAudio)
+		const videoFollowsAudioList = [
+			{ label: 'VFA\\nOn', options: { mode: 'on' } },
+			{ label: 'VFA\\nOff', options: { mode: 'off' } },
+			{ label: 'VFA\\nToggle', options: { mode: 'toggle' } },
+		]
+		addPresetList({
+			category: 'Video Follows Audio',
+			action: 'videoFollowAudio',
+			size: 18,
+			list: videoFollowsAudioList,
+		})
 
 		// Other Actions
-		presets.push(generateWithNoOptions({
+		otherList = [
+			{ label: 'Remote Webview', action: 'remoteWebview' },
+			{ label: 'View Only Mode', action: 'viewOnlyMode' },
+		]
+		addPresetList({
 			category: 'Other Actions',
-			label: 'View Only Mode',
-			size: 14,
-			action: 'viewOnlyMode',
-		}))
+			list: otherList
+		})
 
 		this.setPresetDefinitions(presets)
 	},
