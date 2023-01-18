6aa16df718260f5b4701e869d746da20971fae6b
caleb-wishart <caleb.wish@outlook.com>
2023-01-18 16:27:32 +0000

    Update constants
    Change Object.assign to Object.defineProperty to allow for enumeration

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,28 @@
 module.exports = {
 	initConstants() {
-		Object.assign(this, {
-			DEFAULT_IP: '192.168.1.100',
-			DEFAULT_PORT: '49990',
-			STANDARD: 'standard',
-			ADVANCED: 'advanced',
-			CLASSIC: 'classicAudio',
-			FAIRLIGHT: 'fairlightAudio',
+		Object.defineProperty(this, 'DEFAULT_IP', {
+			value: '192.168.1.100',
+			enumerable: true,
+		})
+		Object.defineProperty(this, 'DEFAULT_PORT', {
+			value: '49990',
+			enumerable: true,
+		})
+		Object.defineProperty(this, 'STANDARD', {
+			value: 'standard',
+			enumerable: true,
+		})
+		Object.defineProperty(this, 'ADVANCED', {
+			value: 'advanced',
+			enumerable: true,
+		})
+		Object.defineProperty(this, 'CLASSIC', {
+			value: 'classicAudio',
+			enumerable: true,
+		})
+		Object.defineProperty(this, 'FAIRLIGHT', {
+			value: 'fairlightAudio',
+			enumerable: true,
 		})
 	},
 }
