fde4d3994ddace6bf839361e2cd874714c234e6d
caleb-wishart <caleb.wish@outlook.com>
2023-01-18 16:08:59 +0000

    Update constants
    Method defineConst no longer exists so changing to Object.assign

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
 module.exports = {
 	initConstants() {
-		this.defineConst('DEFAULT_IP', '192.168.1.100')
-		this.defineConst('DEFAULT_PORT', '49990')
-
-		this.defineConst('STANDARD', 'standard')
-		this.defineConst('ADVANCED', 'advanced')
-
-		this.defineConst('CLASSIC', 'classicAudio')
-		this.defineConst('FAIRLIGHT', 'fairlightAudio')
+		Object.assign(this, {
+			DEFAULT_IP: '192.168.1.100',
+			DEFAULT_PORT: '49990',
+			STANDARD: 'standard',
+			ADVANCED: 'advanced',
+			CLASSIC: 'classicAudio',
+			FAIRLIGHT: 'fairlightAudio',
+		})
 	},
 }
