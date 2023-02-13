b8280e5c1d7f9a92a0cb2947be3b3b94716cc9db
Caleb Wishart <caleb.wish@outlook.com>
2023-02-13 19:39:17 +1000

    Enforce feedback variableId isolation

--- src/feedbacks/outputFeedbacks.js
+++ src/feedbacks/outputFeedbacks.js
@@ -2,20 +2,20 @@ const { generateFeedback } = require('./utils')
 const { option } = require('../actions/utils')
 
 const feedbacksDefinitions = [
-	{ id: 'output_recording', name: 'Output: Recording Status' },
-	{ id: 'output_streaming', name: 'Output: Streaming Status' },
+	{ id: 'output_recording', name: 'Output: Recording Status', variableId: 'output_recording' },
+	{ id: 'output_streaming', name: 'Output: Streaming Status', variableId: 'output_streaming' },
 ]
 
 const getFeedbacks = ({ context }) => {
 	let feedbacks = {}
 
-	feedbacksDefinitions.forEach(({ name, id }) => {
+	feedbacksDefinitions.forEach(({ name, id, variableId }) => {
 		feedbacks = {
 			...feedbacks,
 			[id]: generateFeedback({
 				name,
 				options: [option.onOff({ id, label: 'Status' })],
-				callback: ({ options }) => Boolean(options[id]) === context.getVariableValue(id),
+				callback: ({ options }) => Boolean(options[id]) === context.getVariableValue(variableId),
 			}),
 		}
 	})
