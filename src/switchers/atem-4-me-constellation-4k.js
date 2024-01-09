41476567a225d754ec0a6978082c3af2c0e3e2d3
Adam Tow <adam@tow.com>
2024-01-09 13:52:53 -0800

    * Fix bug where USK Border Hue Set slider would not appear. * Support for ATEM Television Studio 4K8 and 4 M/E Constellation 4K switchers.

---
+++ src/switchers/atem-4-me-constellation-4k.js
@@ -0,0 +1,53 @@
+const { model, audioSource } = require('./types')
+const generator = require('./generators')
+
+const switcher = {
+	id: model.atem4meConstellation4k,
+	label: 'ATEM 4 M/E Constellation 4K',
+	mixEffectBuses: 4,
+	auxBuses: 24,
+	inputs: 40,
+	multiViewers: 4,
+	advancedMultiViewer: true,
+	upstreamKeyers: 16,
+	downstreamKeyers: 4,
+	superSources: 2,
+	superSourceArtBorder: false,
+	macros: 100,
+	mediaPlayers: 4,
+	mediaStills: 64,
+	mediaClips: 4,
+	streaming: false,
+	recording: false,
+	recordISO: false,
+	stinger: true,
+	advancedChromaKeyer: true,
+	fairlightAudio: true,
+}
+
+switcher.videoSources = [
+	...generator.blackSources({ me: switcher.mixEffectBuses }),
+	...generator.videoSources(switcher.inputs, { me: switcher.mixEffectBuses }),
+	...generator.colorBarsSources({ me: switcher.mixEffectBuses }),
+	...generator.colorSources(2, { me: switcher.mixEffectBuses }),
+	...generator.mediaPlayerSources(switcher.mediaPlayers, { me: switcher.mixEffectBuses }),
+	...generator.upstreamKeyMaskSources(switcher.upstreamKeyers),
+	...generator.downstreamKeyMaskSources(switcher.downstreamKeyers),
+	...generator.superSourceSources(switcher.superSources, { me: switcher.mixEffectBuses }),
+	...generator.cleanFeedSources(switcher.mixEffectBuses),
+	...generator.auxiliarySources(switcher.auxBuses),
+	...generator.programSources(switcher.mixEffectBuses),
+	...generator.previewSources(switcher.mixEffectBuses),
+]
+
+switcher.audioSources = [
+	...generator.sdiSources(switcher.inputs),
+	...generator.micSources(1, audioSource.ts),
+	...generator.trsSources(1),
+	...generator.madiSources(32),
+	...generator.mediaPlayerAudioSources(switcher.mediaPlayers),
+]
+
+module.exports = {
+	atem4meConstellation4k: switcher,
+}
